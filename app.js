3fbbf6c1c04f5ccfe8504d3be1b693ed489bd4e1
MrFerrada <marceloferrada94@gmail.com>
2025-09-21 15:09:18 -0300

    Agregar amigo y lista vacia

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 /* Fucionalidades:
-Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar". Listo
 
-Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido. Listo
 
 Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
 
 Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.*/
 
+let amigos = [];
 
+function agregarAmigo(){
+    if (document.getElementById("amigo").value ==""){
+        alert("Por favor ingresa un nombre válido");
+        return;
+    } else{
+        amigos.push(document.getElementById("amigo").value);
+        console.log(amigos);
+        añadirLista();
+    }
+    return;
+}
+
+function añadirLista(){
+    document.getElementById("listaAmigos").value = document.getElementById("amigo").value;
+}
+
+function sortearAmigo(){
+    alert("Esto está de la perra");
+}
 
